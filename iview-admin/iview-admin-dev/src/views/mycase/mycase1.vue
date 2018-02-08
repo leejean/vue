784dc482420ba8d2a5642ea6eddcab6d6f8fe787
易理坚 <ilove_cp3@163.com>
2018-02-08 11:42:24 +0800

    1

---
+++ iview-admin/iview-admin-dev/src/views/mycase/mycase1.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>mycase1</div>
+</template>
+
+<script>
+export default {
+    name: 'mycase1',
+    data () {
+    	return {}
+    },
+    methods: {
+       
+    },
+    mounted () {
+        
+    },
+    watch: {
+        
+    }
+};
+</script>
+
---
+++ iview-admin/iview-admin-dev/src/views/mycase/mycase2.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>mycase2</div>
+</template>
+
+<script>
+export default {
+    name: 'mycase2',
+    data () {
+    	return {}
+    },
+    methods: {
+       
+    },
+    mounted () {
+        
+    },
+    watch: {
+        
+    }
+};
+</script>
+
