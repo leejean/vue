a021617de1323e531cd379c7910de86c7fa81f8b
leejean <ilove_cp3@163.com>
2018-11-06 14:59:55 +0800

    提交

--- iview-admin/iview-admin-dev/src/views/mycase/mycase1.vue
+++ iview-admin/iview-admin-dev/src/views/mycase/mycase1.vue
@@ -1,26 +1,57 @@
 <template>
-    <Tabs value="name1">
-        <TabPane label="标签一" name="name1">标签一的内容1</TabPane>
-        <TabPane label="标签二" name="name2">标签二的内容</TabPane>
-        <TabPane label="标签三" name="name3">标签三的内容</TabPane>
-    </Tabs>
+    <Transfer
+        :data="data1"
+        :target-keys="targetKeys1"
+        :render-format="render1"
+        @on-change="handleChange1"></Transfer>
 </template>
-
 <script>
-export default {
-    name: 'mycase1',
-    data () {
-    	return {}
-    },
-    methods: {
-       
-    },
-    mounted () {
-        
-    },
-    watch: {
-        
+    export default {
+        data () {
+            return {
+                data1: this.getMockData(),
+                targetKeys1: this.getTargetKeys()
+            }
+        },
+        methods: {
+            getMockData () {
+                let mockData = [];
+                for (let i = 1; i <= 20; i++) {
+                    mockData.push({
+                        key: i.toString(),
+                        label: 'Content ' + i,
+                        description: 'The desc of content  ' + i,
+                        disabled: Math.random() * 3 < 1
+                    });
+                }
+                return mockData;
+            },
+            getTargetKeys () {
+                return this.getMockData()
+                        .filter(() => Math.random() * 2 > 1)
+                        .map(item => item.key);
+            },
+            render1 (item) {
+                return item.label;
+            },
+            handleChange1 (newTargetKeys, direction, moveKeys) {
+                console.log(newTargetKeys);
+                console.log(direction);
+                console.log(moveKeys);
+                this.targetKeys1 = newTargetKeys;
+            }
+        }
     }
-};
 </script>
-
+<style>
+	.main .single-page-con {
+	    position: absolute;
+	    top: 100px;
+	    right: 0;
+	    bottom: 0;
+	    overflow: auto;
+	    background-color: #ffffff;
+	    z-index: 1;
+	    transition: left .3s;
+	}
+</style>
